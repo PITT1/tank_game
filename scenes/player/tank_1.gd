88636b8bf1ed9e28f9d509554ef01a30058270b1
PITT1 <petroleo.plta.97@gmail.com>
2025-03-07 22:39:09 -0400

    haciendo que el tanque funcione con el teclado

--- scenes/player/tank_1.gd
+++ scenes/player/tank_1.gd
@@ -1,5 +1,9 @@
 extends VehicleBody3D
 
+@export var ENGINE_POWER = 100
+@export var BRAKE_FORCE = 100
+
+var on_brake: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,3 +13,6 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	
+func _physics_process(delta: float) -> void:
+	engine_force = Input.get_axis("up", "Down") * ENGINE_POWER
