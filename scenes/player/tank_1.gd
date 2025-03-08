ca98b5c1f465e6391b16b87febb721085e1b432f
PITT1 <petroleo.plta.97@gmail.com>
2025-03-07 23:09:56 -0400

    mejorando el moviemiento del tanque

--- scenes/player/tank_1.gd
+++ scenes/player/tank_1.gd
@@ -1,7 +1,11 @@
 extends VehicleBody3D
 
 @export var ENGINE_POWER = 100
+@export var STEER_POWER = 300
 @export var BRAKE_FORCE = 100
+
+@onready var R_wheels = [$R_wheel_3, $R_wheel_2, $R_wheel_1]
+@onready var L_wheels = [$L_wheel_3, $L_wheel_2, $L_wheel_1]
 
 var on_brake: bool = false
 
@@ -15,4 +19,16 @@ func _process(delta: float) -> void:
 	pass
 	
 func _physics_process(delta: float) -> void:
-	engine_force = Input.get_axis("up", "Down") * ENGINE_POWER
+	var steer = Input.get_axis("Left", "Right")
+	var move_dir = Input.get_axis("up", "Down")
+	
+	if steer != 0:
+		for wheel in L_wheels:
+			wheel.engine_force = STEER_POWER * -steer
+		for wheel in R_wheels:
+			wheel.engine_force = STEER_POWER * steer
+	else:
+		for wheel in L_wheels:
+			wheel.engine_force = ENGINE_POWER * move_dir
+		for wheel in R_wheels:
+			wheel.engine_force = ENGINE_POWER * move_dir
