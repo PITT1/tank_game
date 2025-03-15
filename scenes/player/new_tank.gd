e0818b9e2eacc4b1e90f3f5ee8ed5e0dfd40fe91
PITT1 <petroleo.plta.97@gmail.com>
2025-03-15 01:22:09 -0400

    creando orugas para tank_1

--- scenes/player/new_tank.gd
+++ scenes/player/new_tank.gd
@@ -1,12 +1,16 @@
 extends VehicleBody3D
 
-@export var ENGINE_POWER = 200
-@export var STEER_POWER = 100
+@export var ENGINE_POWER = 100
+@export var STEER_POWER = 500
 @export var BRAKE_FORCE = 1
+@export var projectile_velocity = 1000
 
-@onready var R_wheels = [$R_3, $R_2, $R_1]
-@onready var L_wheels = [$L_3, $L_2, $L_1]
+@export var projectile: PackedScene
 
+@onready var R_wheels = [$R_wheel_2, $R_wheel_1]
+@onready var L_wheels = [$L_wheel_2, $L_wheel_1]
+
+# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
@@ -14,9 +18,15 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
+	
 func _physics_process(delta: float) -> void:
 	movement()
+	on_fire()
+	on_brake()
+	
+	if Input.is_action_just_pressed("Jump"):
+		apply_impulse(Vector3.UP * 200)
+	
 
 func movement():
 	var steer = Input.get_axis("Left", "Right")
@@ -32,3 +42,17 @@ func movement():
 			wheel.engine_force = ENGINE_POWER * move_dir
 		for wheel in R_wheels:
 			wheel.engine_force = ENGINE_POWER * move_dir
+
+func on_fire():
+	if Input.is_action_just_pressed("Fire"):
+		var instantia_projectile = projectile.instantiate()
+		instantia_projectile.position = global_position + transform.basis.z + Vector3(0, 0.5, 0)
+		instantia_projectile.set_linear_velocity(transform.basis.z * 20)
+		add_sibling(instantia_projectile)
+
+func on_brake():
+	if Input.is_action_just_pressed("Brake"):
+		for wheel in L_wheels:
+			wheel.brake = BRAKE_FORCE
+		for wheel in R_wheels:
+			wheel.brake = BRAKE_FORCE
